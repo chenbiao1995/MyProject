dbde5ccef4ff960dfa697d8ab57d70cee7adfcf8
chenbiao <chenbiao@msunsoft.com>
2021-06-23 23:01:53 +0800

    ttt

---
+++ src/main/java/com/hozuo/testMybatisDatasource/controller/TestController.java
@@ -0,0 +1,56 @@
+package com.hozuo.testMybatisDatasource.controller;
+
+import com.hozuo.testMybatisDatasource.pojo.User;
+import com.hozuo.testMybatisDatasource.service.TestD1Service;
+import com.hozuo.testMybatisDatasource.service.TestD2Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * TestController
+ * <p>2020/6/23
+ * <p> 测试控制器
+ * @author hozuo
+ * @version 1.0
+ */
+@RestController
+public class TestController {
+
+    private TestD1Service testD1Service;
+
+    private TestD2Service testD2Service;
+
+    @Autowired
+    public void setTestD1Service(TestD1Service testD1Service) {
+        this.testD1Service = testD1Service;
+    }
+
+    @Autowired
+    public void setTestD2Service(TestD2Service testD2Service) {
+        this.testD2Service = testD2Service;
+    }
+
+    @RequestMapping("/d1")
+    public List<User> testD1() {
+        return testD1Service.select();
+    }
+
+    @RequestMapping("/insert")
+    public void insert() {
+        User user = new User();
+        user.setId(new Random().nextInt());
+        user.setUsername("ceshi");
+        user.setPassword("");
+        testD1Service.insert(user);
+        System.out.println("end");
+    }
+
+    @RequestMapping("/d2")
+    public List<User> testD2() {
+        return testD2Service.select();
+    }
+}
---
+++ src/main/java/com/hozuo/testMybatisDatasource/mapper/testD1/TestD1Mapper.java
@@ -0,0 +1,13 @@
+package com.hozuo.testMybatisDatasource.mapper.testD1;
+
+import com.hozuo.testMybatisDatasource.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface TestD1Mapper {
+    List<User> select ();
+
+    void insert(User user);
+}
